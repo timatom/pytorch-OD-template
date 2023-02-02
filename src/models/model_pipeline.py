ac688e6d844ee0b6407dad7db23a5d5f04d8d507
timatom <tim.rollings@protonmail.com>
2023-02-01 23:12:50 -0500

    Created a seperate loss_fn for model training in the model pipeline. Note: the loss function hasn't been tested yet.

--- src/models/model_pipeline.py
+++ src/models/model_pipeline.py
@@ -8,7 +8,7 @@ from src.models.model_factory import ModelFactory
 
 from src.models.builders.model_builder import NeuralNetBuilder
 
-from src.models.detection import mobilenet, resnet
+from src.models.detection import resnet_fpn_retinanet
 
 # Built-in models
 from torchvision.models.detection import (fasterrcnn_resnet50_fpn, 
@@ -20,17 +20,47 @@ from torchvision.models.detection import (fasterrcnn_resnet50_fpn,
                                           keypointrcnn_resnet50_fpn,
                                           ssdlite320_mobilenet_v3_large)
 
+# import loss functions
+import torch.nn.functional as F
 
+# TODO: Need to implement a seperate loss class that will be used to calculate the loss of the model.
+# Note: Make sure to do so in a way that is compatible with the `ModelFactory` class.
+# Note: This seperation should still make make the codebase modular, maintainable, and easy to understand.
+# Note: Some ways to achieve this are by using clear contracts, documenting the relationship between 
+#       the loss function and the model, and using wrapper classes
+def loss_fn(output, targets):
+    '''
+    This function is used to calculate the loss of the model.
+    Args:
+        output: The output of the model.
+        targets: The ground truth labels and bounding boxes.
+    Returns:
+        losses (dict): A dictionary containing the loss values.
+    '''
+    losses = {}
+    
+    loc_loss = F.smooth_l1_loss(output["bbox_regression"], targets["bbox_regression"])
+    conf_loss = F.binary_cross_entropy_with_logits(output["cls_logits"], targets["cls_logits"])
+    
+    losses["loc_loss"] = loc_loss
+    losses["conf_loss"] = conf_loss
+    
+    return losses
 
 # Define the data path
 data_path = Path("/workspaces/pytorch-od-template/datasets/v1/")
 
 # Get model
-model = mobilenet()
+model = resnet_fpn_retinanet()
+
+# model = fasterrcnn_resnet50_fpn(weights = "COCO_V1", pretrained=True)
+
+# Get loss function
+loss_fn = loss_fn
 
 # Create instances of the classes
 data_factory = DataFactory(data_path)
-model_factory = ModelFactory(data_factory, model)
+model_factory = ModelFactory(data_factory, model, loss_fn)
 
 # Train the model
 model = model_factory.train(epochs = 2)
