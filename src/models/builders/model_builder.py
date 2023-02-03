b7e73fb822e95739dd48b17207477c12a4c11ca4
timatom <tim.rollings@protonmail.com>
2023-02-02 19:39:31 -0500

    Updated model-builder class.

--- src/models/builders/model_builder.py
+++ src/models/builders/model_builder.py
@@ -1,26 +1,35 @@
 import torch.nn as nn
 
-class NeuralNetBuilder:
+from src.models.builders.base_model_builder import BaseNeuralNetBuilder
+
+def forward(x, targets=None):
+        # pass the inputs through the backbone to get the feature maps
+        x = self._model.backbone(x)
+        
+        # pass the feature maps through the neck to get the feature maps
+        x = self._model.neck(x)
+        
+        # pass the feature maps through the head to get the predictions
+        x = self._model.head(x, targets)
+             
+        return x
+
+class NeuralNetBuilder(BaseNeuralNetBuilder):
     def __init__(self):
-        self.backbone = None
-        self.neck = None
-        self.head = None
-        
-    def set_backbone(self, backbone):
-        self.backbone = backbone
+        super().__init__()
+        self._model = nn.Sequential()
+
+    def build_backbone(self, backbone):
+        self._model.add_module("backbone", backbone)
+
+    def build_neck(self, neck):
+        self._model.add_module("neck", neck)
+
+    def build_head(self, head):
+        self._model.add_module("head", head)
     
-    def set_neck(self, neck):
-        self.neck = neck
-    
-    def set_head(self, head):
-        self.head = head
-        
-    def build_model(self):
-        '''
-        Builds the neural network.
-        '''
-        return nn.Sequential(
-            self.backbone.build(),
-            self.neck.build(),
-            self.head.build()
-        )
+    def build_forward(self, x, targets=None):
+        self._model.add_module("forward", forward)
+
+    def get_model(self):
+        return self._model
